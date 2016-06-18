9a6b67739ff12e588775ca717e1fb171dad0d613
Alexander Schwarz <alderon@aikq.de>
2016-06-18 12:40:14 +0200

    encapsulate the parser state enum

--- altplay/parser.hpp
+++ altplay/parser.hpp
@@ -4,16 +4,15 @@
 
 namespace irc_lib
 {
-	enum parser_states_enum
-	{ START, SERVERMESSAGE, MESSAGE, HOST, IDENT, DONE_CLIENT, DONE_SERVER, PARSE_ERROR };
-
 	class parser
 	{
 	public:
 		// strong error guarantee, won't change the original string and will raise an exception
+		// parses the raw string via a finite state machine and distinguishes between server and client messages
 		message_struct handle_input(const std::string& str);
 
 	private:
-		parser_states_enum current_state_{START};
+		enum parser_states { START, SERVERMESSAGE, MESSAGE, HOST, IDENT, DONE_CLIENT, DONE_SERVER, PARSE_ERROR };
+		parser_states current_state_{START};
 	};
 } // end of ns irc_lib
