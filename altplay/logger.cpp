b3274157950d7fb6be67aaff8ec5736194ad2c46
Alexander Schwarz <alderon@aikq.de>
2016-06-19 12:27:03 +0200

    prevent buffering for debugging purposes

--- altplay/logger.cpp
+++ altplay/logger.cpp
@@ -3,6 +3,9 @@
 
 altplay::logger::logger(std::string path)
 {
+#ifdef DEBUG_ON
+	this->file_.rdbuf()->pubsetbuf(0, 0);
+#endif
 	this->file_.open(path, std::ios::app | std::ios::in | std::ios::out);
 	if (!file_.is_open()) throw std::runtime_error("logger was unable to open the provided file.");
 }
