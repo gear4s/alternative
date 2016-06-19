81f00211e7cbdf96ca87e9db499aee35fbb25917
Alexander Schwarz <alderon@aikq.de>
2016-06-19 11:44:53 +0200

    more meaningful exception message when the logger constructor fails

--- altplay/logger.cpp
+++ altplay/logger.cpp
@@ -4,7 +4,7 @@
 altplay::logger::logger(std::string path)
 {
 	this->file_.open(path, std::ios::app | std::ios::in | std::ios::out);
-	if (!file_.is_open()) throw std::runtime_error("Unable to open the file.");
+	if (!file_.is_open()) throw std::runtime_error("logger was unable to open the provided file.");
 }
 
 void altplay::logger::add_entry(const std::string& str)
