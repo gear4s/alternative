3836d3c09fe593af4146adf25e68d2cef32b03a9
Alexander Schwarz <alderon@aikq.de>
2016-06-19 10:40:03 +0200

    adding header

--- altplay/parser.hpp
+++ altplay/parser.hpp
@@ -9,6 +9,8 @@ namespace irc_lib
 		// strong error guarantee, won't change the original string and will raise an exception
 		message_struct handle_input(const std::string& str) const;
 		// returns true when nicks match, case insensitive
+		bool compare_nicks(std::string nick1, std::string nick2);
 		// returns true when channel names match, case insensitive
+		bool compare_channel_names(std::string channel1, std::string channel2);
 	};
 } // end of ns irc_lib
