288e4b6732d6a9ec411b7caf3fadbc4523488d90
Alexander Schwarz <alexander@schwarz-regensburg.de>
2016-07-10 17:06:07 +0200

    more meaningful error messages from database_handler

--- altplay/database_handler.cpp
+++ altplay/database_handler.cpp
@@ -12,16 +12,16 @@ altplay::database_handler::database_handler() {
         try {
             port_ = std::stoi(temp.data());
             if (port_ < 0 || port_ > 65535) {
-                throw std::runtime_error("Port is out of range");
+                throw std::runtime_error("db_handler: port is out of range");
             }
         } catch (const std::exception &e) {
-            throw std::runtime_error("Not a port number");
+            throw std::runtime_error("db_handler: not a port number");
         }
         user_ = config_map.at("db_user");
         password_ = config_map.at("db_password");
         database_ = config_map.at("db_database");
         if (!con_.connect(database_.data(), host_.data(), user_.data(), password_.data(), port_)) {
-            throw std::runtime_error("Couldn't connect to database");
+            throw std::runtime_error("db_handler: couldn't connect to database");
         }
 
     } catch (const std::exception &e) {
