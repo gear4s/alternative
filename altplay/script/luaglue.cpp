581fbf57d7bb943c4f06b081252585f8dc958723
Aaron Marais <aaro@graphox.us>
2016-07-13 20:25:44 +0200

    ease for new hook system

--- altplay/script/luaglue.cpp
+++ altplay/script/luaglue.cpp
@@ -11,11 +11,10 @@ namespace altplay {
   namespace script {
     namespace lua {
       lua_State *L;
-      
+
       std::list<irchook> _hooks;
 
-      void hook(std::string command, LuaRef function)
-      {
+      void hook(std::string command, LuaRef function) {
         irchook hook(L);
 
         hook.command = command;
@@ -25,42 +24,34 @@ namespace altplay {
       }
 
       auto init() -> std::tuple<int, const char *> {
-        auto quitter = [](int) { altplay::quit = true; };
-        signal(SIGTERM, quitter);
-        signal(SIGINT, quitter);
         L = luaL_newstate();
         luaL_openlibs(L);
 
         getGlobalNamespace(L)
           .beginClass<message_struct>("message_struct")
 #define Data(n) addData(#n, &message_struct::n)
-            .Data(nick)
-            .Data(hostmask)
-            .Data(ident)
-            .Data(prefix)
-            .Data(params)
-            .Data(target)
-            .Data(message)
-            .Data(is_server_message)
+          .Data(nick)
+          .Data(hostmask)
+          .Data(ident)
+          .Data(prefix)
+          .Data(params)
+          .Data(target)
+          .Data(message)
+          .Data(is_server_message)
 #undef setProp
           .endClass()
 
           .beginNamespace("bot")
-            .addFunction("hook", &hook)
-            .addProperty("quit", +[] { return altplay::quit; }, +[](bool v) {
+          .addFunction("hook", &hook)
+          .addProperty("quit", +[] { return altplay::quit; }, +[](bool v) {
               if (altplay::quit && !v) luaL_error(L, "Cannot abort a quit");
               altplay::quit = v;
             })
-          .endNamespace()
-          .beginNamespace("irc")
-            .beginNamespace("reply")
-            .endNamespace()
-            .beginNamespace("error")
-            .endNamespace()
           .endNamespace();
-#define addEnum(n)    lua_pushstring(L, #n); lua_pushnumber(L, n); lua_rawset(L, -3)
-        lua_getglobal(L, "irc");
-        lua_getfield(L, -1, "reply");
+#define addEnum(n)    lua_pushliteral(L, #n); lua_pushnumber(L, n); lua_rawset(L, -3)
+        lua_newtable(L);
+        lua_pushliteral(L, "reply");
+        lua_newtable(L);
           addEnum(WELCOME); addEnum(YOURHOST); addEnum(CREATED); addEnum(MYINFO); addEnum(BOUNCE);
           addEnum(TRACELINK); addEnum(TRACECONNECTING); addEnum(TRACEHANDSHAKE); addEnum(TRACEUNKNOWN);
           addEnum(TRACEOPERATOR); addEnum(TRACEUSER); addEnum(TRACESERVER); addEnum(TRACESERVICE);
@@ -80,7 +71,10 @@ namespace altplay {
           addEnum(MOTD); addEnum(ENDOFINFO); addEnum(MOTDSTART); addEnum(ENDOFMOTD); addEnum(YOUREOPER);
           addEnum(REHASHING); addEnum(YOURESERVICE); addEnum(TIME); addEnum(USERSSTART); addEnum(USERS);
           addEnum(ENDOFUSERS); addEnum(NOUSERS);
-        lua_getfield(L, -2, "error");
+        lua_settable(L, -3);
+
+        lua_pushliteral(L, "error");
+        lua_newtable(L);
           addEnum(NOSUCHNICK); addEnum(NOSUCHSERVER); addEnum(NOSUCHCHANNEL); addEnum(CANNOTSENDTOCHAN);
           addEnum(TOOMANYCHANNELS); addEnum(WASNOSUCHNICK); addEnum(TOOMANYTARGETS); addEnum(NOSUCHSERVICE);
           addEnum(NOORIGIN); addEnum(NORECIPIENT); addEnum(NOTEXTTOSEND); addEnum(NOTOPLEVEL);
@@ -95,12 +89,18 @@ namespace altplay {
           addEnum(BANLISTFULL); addEnum(NOPRIVILEGES); addEnum(CHANOPPRIVSNEEDED); addEnum(CANTKILLSERVER);
           addEnum(RESTRICTED); addEnum(UNIQOPPRIVSNEEDED); addEnum(NOOPERHOST); addEnum(UMODEUNKNOWNFLAG);
           addEnum(USERSDONTMATCH);
+        lua_settable(L, -3);
+
+        lua_pushliteral(L, "hook");
+        lua_newtable(L);
+        lua_settable(L, -3);
+        lua_setglobal(L, "irc");
 #undef addEnum
 
         if (!L) {
           return std::make_tuple(0, "");
         }
-        if (luaL_loadfile(L, "script.lua")){
+        if (luaL_loadfile(L, "script/lua/bootstrap.lua")) {
           return std::make_tuple(1, lua_tostring(L, -1));
         }
         lua_call(L, 0, 0);
