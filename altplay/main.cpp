55a28adf38f827e5d55ac932dce639db7fd4a5c7
Aaron Marais <aaron@graphox.us>
2016-07-22 18:25:15 +0200

    added CTRL-C to stop bot move lua script to scripts/lua/bootstrap.lua

--- altplay/main.cpp
+++ altplay/main.cpp
@@ -6,11 +6,17 @@
 using namespace std;
 using asio::ip::tcp;
 
+namespace altplay {
+  extern bool quit;
+}
+
 int main( )
 {
     try {
         std::setlocale ( LC_ALL, "en_US.UTF-8" );
         asio::io_service io_service_;
+        asio::signal_set signals(io_service_, SIGINT, SIGTERM);
+        signals.async_wait(std::bind(&asio::io_service::stop, &io_service_));
         altplay::bot bot( io_service_ );
         io_service_.run( );
     }
