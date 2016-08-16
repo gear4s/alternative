042117ad17eedb9fc21ffab651e44f14533e1905
Aaron Narwhaal <aaronleem@gmail.com>
2016-08-16 10:27:33 +0200

    Chane how CTCP replies are sent

--- altplay/script/lua/bootstrap.lua
+++ altplay/script/lua/bootstrap.lua
@@ -26,21 +26,22 @@ bot.hook("CTCP", function(msg)
   if msg.message ~= "ACTION" then
     irc.notice(msg.nick, (setmetatable({
       prototype={
-        VERSION="VERSION AltPlay IRC Bot (C) 2016 AltPlay Community Developers",
-        FINGER="FINGER :o MEANIE!",
-        TIME=function() return "TIME " .. os.date("%H:%M:%S %a %d %b %Y") end,
+        VERSION="AltPlay IRC Bot (C) 2016 AltPlay Community Developers",
+        FINGER=":o MEANIE!",
+        TIME=function() return os.date("%H:%M:%S %a %d %b %Y") end,
         UPTIME=function()
           local d = os.date("!*t", os.difftime(os.time(), startTime))
-          return "UPTIME " .. d.hour .. " hours; " .. d.min .. " minutes; " .. d.sec .. " seconds"
+          return d.hour .. " hours; " .. d.min .. " minutes; " .. d.sec .. " seconds"
         end,
-        SOURCE="SOURCE http://altplay.net/",
-        SAUCE="SAUCE http://altplay.net/",
-        USERINFO="USERINFO These gosh darn CTCPs",
-        CLIENTINFO="CLIENTINFO CLIENTINFO FINGER SOURCE SAUCE VERSION TIME UPTIME USERINFO"
+        SOURCE="http://altplay.net/",
+        SAUCE="http://altplay.net/",
+        USERINFO="These gosh darn CTCPs",
+        CLIENTINFO="CLIENTINFO FINGER SOURCE SAUCE VERSION TIME UPTIME USERINFO"
       }
     }, {
       __index = function(t,k)
-        return "\001" .. (type(rawget(t,"prototype")[k]) == "function" and rawget(t,"prototype")[k]() or rawget(t,"prototype")[k] or "ERRMSG Invalid CTCP request") .. "\001"
+        local n = rawget(t, "prototype")[k]
+        return "\001" .. (n ~= nil and (k.." ")) .. (type(n) == "function" and n() or n or "ERRMSG Invalid CTCP request") .. "\001"
       end
     }))[msg.message:split(" ")[1]])
   end
